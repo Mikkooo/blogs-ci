d37daaacd17ce4dc0b558dce37fa5a008d3c508c
Mikko Pirhonen <mikko.pirhonen@visy.fi>
2021-02-01 18:19:05 +0200

    fix linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,10 @@ module.exports = {
     commonjs: true,
     es2021: true,
     jest: true,
+    node: true
   },
   extends: [
-    'airbnb-base',
+    'eslint:recommended'
   ],
   parserOptions: {
     ecmaVersion: 12,
