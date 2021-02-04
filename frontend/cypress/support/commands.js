6bf015ee645482fcb9d8b29842097da440a91595
Mikko Pirhonen <mikko.pirhonen@visy.fi>
2021-02-04 19:40:57 +0200

    pls3

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -27,7 +27,7 @@ Cypress.Commands.add('login', ({ username, password }) => {
   cy.request('POST', 'http://localhost:3001/api/login', { username, password })
     .then(({ body }) => {
       localStorage.setItem('loggedUser', JSON.stringify(body));
-      cy.visit('http://localhost:3000');
+      cy.visit('http://localhost:3001');
     });
 });
 Cypress.Commands.add('addBlog', (blog) => {
